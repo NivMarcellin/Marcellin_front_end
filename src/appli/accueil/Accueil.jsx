77578f942e39e4bcaf826c13bef2513b19a1d69b
Your Name <you@example.com>
2023-10-17 11:48:06 +0200

    Front-end

--- src/appli/accueil/Accueil.jsx
+++ src/appli/accueil/Accueil.jsx
@@ -3,16 +3,17 @@ import Title from "../../component/Title";
 import Footer from "../../component/Footer";
 const Accueil = () => {
   return (
-    <div className="composent">
+    <div>
       <div className="content"></div>
-      <div className="childcomposent">
-        <Title />
-        <div className="choose">
-        
-          <button class="button">Connexion</button>
-          <button class="button">Inscription</button>
+      <div className="composent">
+        <div className="childcomposent">
+          <Title />
+          <div className="choose">
+            <button class="button">Connexion</button>
+            <button class="button">Inscription</button>
+          </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
     </div>
   );
--- src/appli/signin/Signin.jsx
+++ src/appli/signin/Signin.jsx
@@ -6,38 +6,41 @@ const Signin = () => {
   return (
     <div>
       <div className="content1"></div>
-      <Title />
-      <div className="sign">
-        <div class="field">
-          <p class="control has-icons-left has-icons-right">
-            <input class="input" type="email" placeholder="Email" />
-            <span class="icon is-small is-left">
-              <i class="fas fa-envelope"></i>
-            </span>
-            <span class="icon is-small is-right">
-              <i class="fas fa-check"></i>
-            </span>
-          </p>
-        </div>
-        <div class="field">
-          <p class="control has-icons-left">
-            <input class="input" type="password" placeholder="Password" />
-            <span class="icon is-small is-left">
-              <i class="fas fa-lock"></i>
-            </span>
-          </p>
-        </div>
-        <div className="identifier">
-        <label class="checkbox">
-            <input type="checkbox"/>
-            Remember me
-          </label>
-        <div class="field">
-          <p class="control">
-            <button class="button">identifier</button>
-          </p>
-          
-        </div>
+      <div className="component">
+        <Title />
+        <div className="sign">
+          <div className="texte">
+            <div class="field">
+              <p class="control has-icons-left has-icons-right">
+                <input class="input" type="email" placeholder="Email" />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-envelope"></i>
+                </span>
+                <span class="icon is-small is-right">
+                  <i class="fas fa-check"></i>
+                </span>
+              </p>
+            </div>
+            <div class="field">
+              <p class="control has-icons-left">
+                <input class="input" type="password" placeholder="Password" />
+                <span class="icon is-small is-left">
+                  <i class="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="identifier">
+            <label class="checkbox">
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <div class="field">
+              <p class="control">
+                <button class="button">identifier</button>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <Footer />
